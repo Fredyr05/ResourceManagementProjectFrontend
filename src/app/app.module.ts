e522c8ca1ee2be68203cb3fc65a8d4b7d0fbe875
Fredy Ramirez <fredyjr0@gmail.com>
2019-09-14 11:15:46 -0400

    Added Header, Sidebar, Project, and Resource components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProjectComponent } from './project/project.component';
+import { ResourceComponent } from './resource/resource.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ProjectComponent,
+    ResourceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  opened = false;
+  buttonText = '>';
+
+  openClose(){
+    this.opened = !this.opened;
+    if(this.buttonText === '>'){
+        this.buttonText = '<';
+    }
+    else{
+        this.buttonText = '>';
+    }
+  }
+}
