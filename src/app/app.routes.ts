ab214c906ebafe99f7c5f5b3c9f59ebe52cf283b
John Bolanos <johnbola@buffalo.edu>
2019-09-16 15:52:28 -0400

    Created template for login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {LoginComponent} from './login/login/login.component';
 import { ProjectComponent } from './project/project.component';
+import { from } from 'rxjs';
 
 const appRoutes: Routes = [
   //{ path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'project', component: ProjectComponent}
+  { path: 'project', component: ProjectComponent},
+  { path: 'login', component: LoginComponent}
   /*children: [
     { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent },
