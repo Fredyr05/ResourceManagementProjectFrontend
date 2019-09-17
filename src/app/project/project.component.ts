d7b93ce2d572d5e04575fbecf23490568fa51d15
Fredy Ramirez <fredyjr0@gmail.com>
2019-09-17 15:23:20 -0400

    Updated Project page to have more client requirements

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 export interface Project {
+    projId: number;
     projName: String;
 }
 
@@ -14,11 +15,41 @@ export class ProjectComponent {
     projects: Project[] = [];
     project: Project;
 
-    addProject(projName:String){
+    columnNames: String[];
+    columnData;
+    selected;
+
+    addProject(projId:number,projName:String){
+        if(this.projects.length == 0){
+            this.selected = projName;
+        }
         this.project = {
+            projId,
             projName
         };
         this.projects.push(this.project);
     }
   
+    fillDummyData(){
+        this.columnNames = ["Resource", "Code"];
+        this.columnData = [{
+            resource: 'Wood',
+            code: '00 00 00'
+        },{
+            resource: 'Metal',
+            code: '11 11 11'
+        },{
+            resource: 'Coal',
+            code: '22 22 22'
+        },{
+            resource: 'Paper',
+            code: '33 33 33'
+        },{
+            resource: 'Diamond',
+            code: '44 44 44'
+        }]
+
+        this.addProject(1,"Project 1");
+        this.addProject(2,"Project 2");
+    }
 }
