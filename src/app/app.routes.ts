7437ce19a697d2fb6d68931698b24082ffba14fc
jchen81 <jchen81@stevens.edu>
2019-09-18 10:28:54 -0400

    add a path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,15 @@ import {LoginComponent} from './login/login/login.component';
 import { ProjectComponent } from './project/project.component';
 import { from } from 'rxjs';
 import { FormulaComponent } from './formula/formula.component';
+import { TemplateComponent } from './template/template.component';
 
 const appRoutes: Routes = [
   //{ path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'project', component: ProjectComponent},
   { path: 'login', component: LoginComponent},
-  {path: 'formula',component: FormulaComponent}
+  {path: 'formula',component: FormulaComponent},
+  {path: 'template',component: TemplateComponent}
   /*children: [
     { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent },
