d79b621bbf4536bb11ebc5826e27722da508615b
Fredy Ramirez <fredyjr0@gmail.com>
2019-09-21 16:26:44 -0400

    Made sidebar button stay at position, added data to resource page, fixed project page layout to fit with button update

--- src/app/resource/resource.component.ts
+++ src/app/resource/resource.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-resource',
   templateUrl: './resource.component.html',
   styleUrls: ['./resource.component.css']
 })
-export class ResourceComponent {
+export class ResourceComponent implements OnInit{
+
+  constructor(private httpService: HttpService){}
+
+  resources: any;
+
+  ngOnInit(){
+    this.getAllResources();
+  }
+
+  getAllResources(){
+    this.httpService.getAllResources().subscribe(
+        res => {this.resources = res;}
+    );
+  }
   
 }
